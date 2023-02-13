c111be639482b1c441021a241fde1b41385fcef3
Savy <savy.novachez011@gmail.com>
2023-02-13 11:07:53 +0530

    updated Treesitter & Lualine configs

--- lua/lualine-config/init.lua
+++ lua/lualine-config/init.lua
@@ -5,8 +5,8 @@ require('lualine').setup {
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
     disabled_filetypes = {
-      statusline = {"txt"},
-      winbar = {"txt"},
+      statusline = {"alpha"},
+      winbar = {"alpha"},
     },
     ignore_focus = {},
     always_divide_middle = true,
