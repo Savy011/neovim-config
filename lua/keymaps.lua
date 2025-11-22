69236664010ba5fb385f1524c68ff60b5257ae56
Savy <savy.novachez011@proton.me>
2025-11-22 12:29:49 +0530

    add keymaps

---
+++ lua/keymaps.lua
@@ -0,0 +1,6 @@
+vim.keymap.set("n", "<C-s>", ":w<CR>")
+
+vim.keymap.set("n", "<Left>", "<Nop>")
+vim.keymap.set("n", "<Right>", "<Nop>")
+vim.keymap.set("n", "<Up>", "<Nop>")
+vim.keymap.set("n", "<Down>", "<Nop>")
