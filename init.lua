6d2b91d541169305841e713b032e771873ac4a8f
Savy <savy.novachez011@proton.me>
2025-11-23 15:57:39 +0530

    i just need a single file..

--- init.lua
+++ init.lua
@@ -1,21 +1,56 @@
-require("opts")
-require("keymaps")
-require("autocmds")
+-- Opts
+vim.o.hlsearch = false
+vim.wo.number = true
+vim.o.termguicolors = true
+
+vim.wo.signcolumn = "yes"
+vim.opt.scrolloff = 10
+
+vim.o.mouse = "a"
+vim.o.clipboard = "unnamedplus"
+
+vim.o.breakindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.smartindent = true
+
+vim.o.undofile = true
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+
+vim.o.completeopt = "menuone,noselect"
+
+-- Keymaps
+local map = vim.keymap
+
+map.set("n", "<C-s>", ":w<CR>")
+
+map.set("n", "<Left>", "<Nop>")
+map.set("n", "<Right>", "<Nop>")
+map.set("n", "<Up>", "<Nop>")
+map.set("n", "<Down>", "<Nop>")
+
+map.set("v", "<", "<gv")
+map.set("v", ">", ">gv")
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -24,12 +59,199 @@ vim.opt.rtp:prepend(lazypath)
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
-
 -- Setup lazy.nvim
 require("lazy").setup({
 	{ "windwp/nvim-autopairs", event = "InsertEnter", config = true },
-	{ "neovim/nvim-lspconfig" },
 	{ "onsails/lspkind.nvim" },
-	{ import = "plugins" }
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				transparent_background = true,
+				show_end_of_buffer = true,
+				integrations = {
+					cmp = true,
+					treesitter = true,
+					native_lsp = {
+						enabled = true,
+					},
+				},
+			})
+
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"hrsh7th/cmp-emoji",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		config = function()
+			local cmp = require("cmp")
+
+			local has_words_before = function()
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0
+					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
+
+			cmp.setup({
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "buffer" },
+					{ name = "path" },
+					{ name = "emoji" },
+				},
+				view = {
+					entries = { name = "custom", selection_order = "near_cursor" },
+				},
+				window = {
+					completion = {
+						winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+						col_offset = -3,
+						side_padding = 0,
+					},
+				},
+				formatting = {
+					fields = { "kind", "abbr", "menu" },
+					format = function(entry, vim_item)
+						local kind =
+							require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
+						local strings = vim.split(kind.kind, "%s", { trimempty = true })
+						kind.kind = " " .. (strings[1] or "") .. " "
+						kind.menu = "    (" .. (strings[2] or "") .. ")"
+
+						return kind
+					end,
+				},
+				experimental = { ghost_text = { hl_group = "CmpGhostText" } },
+				completion = {
+					keyword_length = 4,
+					completeopt = "menu,menuone,noselect",
+				},
+				mapping = {
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.confirm({ select = true })
+						elseif vim.snippet and vim.snippet.active({ direction = 1 }) then
+							vim.schedule(function()
+								vim.snippet.jump(1)
+							end)
+						elseif has_words_before() then
+							cmp.complete()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<C-k>"] = cmp.mapping.select_prev_item(),
+					["<C-j>"] = cmp.mapping.select_next_item(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<Esc>"] = cmp.mapping.abort(),
+				},
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				sync_install = false,
+				modules = {},
+				ensure_installed = {
+					"html",
+					"css",
+					"lua",
+					"astro",
+					"go",
+					"javascript",
+					"typescript",
+					"tsx",
+					"svelte",
+					"gleam",
+					"markdown",
+				},
+				ignore_install = {},
+				auto_install = true,
+				indent = {
+					enable = true,
+				},
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"folke/lazydev.nvim",
+			ft = "lua",
+			opts = {
+				diagnostics = {
+					virtual_text = true,
+				},
+				library = {
+					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				},
+			},
+		},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+		opts = {
+			ensure_installed = { "lua_ls", "gopls", "ts_ls", "svelte", "zls" },
+			auto_install = true,
+			automatic_enable = true,
+		},
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+			},
+			format_on_save = {
+				timeout_ms = 2000,
+				lsp_format = "fallback",
+			},
+			formatters = {
+				eslint_d = {
+					command = "eslint_d",
+					args = { "--fix" },
+				},
+			},
+		},
+	},
 })
 
+vim.diagnostic.config({
+	virtual_text = true,
+})
+
+-- Autocmds
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
+})
