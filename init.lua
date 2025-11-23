3327aac75d5b91276e87dd15d394329aed850974
Savy <savy.novachez011@proton.me>
2025-11-23 19:12:08 +0530

    appearance stuff

--- init.lua
+++ init.lua
@@ -1,41 +1,22 @@
 -- Opts
-vim.o.hlsearch = false
-vim.wo.number = true
-vim.o.termguicolors = true
-
-vim.wo.signcolumn = "yes"
+vim.opt.hlsearch = false
+vim.opt.number = true
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
 vim.opt.scrolloff = 10
-
-vim.o.mouse = "a"
-vim.o.clipboard = "unnamedplus"
-
-vim.o.breakindent = true
+vim.opt.mouse = "a"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cursorline = true
+vim.opt.breakindent = true
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 vim.opt.smartindent = true
-
-vim.o.undofile = true
-
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
-vim.o.updatetime = 250
-vim.o.timeoutlen = 300
-
-vim.o.completeopt = "menuone,noselect"
-
--- Keymaps
-local map = vim.keymap
-
-map.set("n", "<C-s>", ":w<CR>")
-
-map.set("n", "<Left>", "<Nop>")
-map.set("n", "<Right>", "<Nop>")
-map.set("n", "<Up>", "<Nop>")
-map.set("n", "<Down>", "<Nop>")
-
-map.set("v", "<", "<gv")
-map.set("v", ">", ">gv")
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.completeopt = "menuone,noselect"
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -59,10 +40,12 @@ vim.opt.rtp:prepend(lazypath)
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
 -- Setup lazy.nvim
 require("lazy").setup({
 	{ "windwp/nvim-autopairs", event = "InsertEnter", config = true },
 	{ "onsails/lspkind.nvim" },
+	{ "numToStr/Comment.nvim" },
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
@@ -148,10 +131,11 @@ require("lazy").setup({
 							fallback()
 						end
 					end, { "i", "s" }),
-					["<C-k>"] = cmp.mapping.select_prev_item(),
-					["<C-j>"] = cmp.mapping.select_next_item(),
+					["<S-k>"] = cmp.mapping.select_prev_item(),
+					["<S-j>"] = cmp.mapping.select_next_item(),
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
 					["<Esc>"] = cmp.mapping.abort(),
+					["<C-e>"] = cmp.mapping.abort(),
 				},
 			})
 		end,
@@ -216,6 +200,10 @@ require("lazy").setup({
 		},
 	},
 	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
 		"stevearc/conform.nvim",
 		opts = {
 			formatters_by_ft = {
@@ -233,14 +221,111 @@ require("lazy").setup({
 			},
 		},
 	},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+		lazy = false,
+		keys = {
+			{
+				"<leader>ft",
+				function()
+					require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+				end,
+				desc = "Explore Neotree (project)",
+			},
+		},
+		opts = {
+			close_if_last_window = true,
+			window = {
+				position = "right",
+				width = 40,
+			},
+			filesystem = {
+				filtered_items = {
+					always_show_by_pattern = {
+						".env*",
+					},
+				},
+				follow_current_file = {
+					enabled = true,
+				},
+			},
+		},
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		opts = {
+			scope = { enabled = true },
+		},
+	},
+	{
+		"akinsho/bufferline.nvim",
+		version = "*",
+		dependencies = "nvim-tree/nvim-web-devicons",
+		opts = {
+			options = {
+				diagnostics = "nvim_lsp",
+				indicator = {
+					separator_style = "slope",
+				},
+			},
+		},
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "catppuccin",
+					component_separators = "|",
+					section_separators = { left = "", right = "" },
+				},
+				sections = {
+
+					lualine_x = { "encoding", "filetype" },
+					lualine_y = { "lsp_status" },
+				},
+			})
+		end,
+	},
 })
 
 vim.diagnostic.config({
 	virtual_text = true,
 })
 
--- Autocmds
+-- Keymaps
+local map = vim.keymap
 
+map.set("n", "<C-s>", ":w<CR>")
+map.set("n", "<C-q>", ":bd<CR>")
+map.set("n", "<C-h>", ":bprevious<CR>")
+map.set("n", "<C-l>", ":bnext<CR>")
+
+map.set("n", "<Left>", "<Nop>")
+map.set("n", "<Right>", "<Nop>")
+map.set("n", "<Up>", "<Nop>")
+map.set("n", "<Down>", "<Nop>")
+
+map.set("v", "<", "<gv")
+map.set("v", ">", ">gv")
+
+map.set("n", "<leader>ff", require("telescope.builtin").find_files, { desc = "Find files" })
+map.set("n", "<leader>fg", require("telescope.builtin").live_grep, { desc = "Live grep" })
+map.set("n", "<leader>fb", require("telescope.builtin").buffers, { desc = "List open buffers" })
+
+map.set("n", "<leader>v", function()
+	vim.cmd("vsplit | enew")
+end, { desc = "Vertical split and open new buffer" })
+
+-- Autocmds
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
 	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
@@ -255,3 +340,17 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 		require("conform").format({ bufnr = args.buf })
 	end,
 })
+
+vim.api.nvim_set_hl(0, "CmpGhostText", {
+	italic = true,
+	link = "Comment",
+	blend = 40,
+})
+
+local catp_text = "#cdd6f4"
+vim.api.nvim_set_hl(0, "LineNrAbove", { fg = catp_text, blend = 80 })
+vim.api.nvim_set_hl(0, "LineNr", { fg = catp_text, blend = 40 })
+vim.api.nvim_set_hl(0, "LineNrBelow", { fg = catp_text, blend = 80 })
+
+vim.api.nvim_set_hl(0, "CursorLine", { bg = "#313244" })
+vim.api.nvim_set_hl(0, "Pmenu", { bg = "#313244" })
