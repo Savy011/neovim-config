ebdbd6b2c2c9fa1678c9850d1ebabc92fd997d5d
Savy <savy.novachez011@proton.me>
2025-11-23 19:30:36 +0530

    add alpha

--- init.lua
+++ init.lua
@@ -318,6 +318,63 @@ require("lazy").setup({
 			},
 		},
 	},
+	{
+		"goolord/alpha-nvim",
+		event = "VimEnter",
+		opts = function()
+			local dashboard = require("alpha.themes.dashboard")
+			local logo = [[ _       __                     _
+| |     / /__  ___  ___        (_)___ ___
+| | /| / / _ \/ _ \/ _ \______/ / __ `__ \
+| |/ |/ /  __/  __/  __/_____/ / / / / / /
+|__/|__/\___/\___/\___/     /_/_/ /_/ /_/]]
+
+			dashboard.section.header.val = vim.split(logo, "\n")
+			dashboard.section.buttons.val = {
+				dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+				dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+				dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+				dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
+				dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
+				dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+			}
+
+			for _, button in ipairs(dashboard.section.buttons.val) do
+				button.opts.hl = "AplhaButtons"
+				button.opts.hl_shortcut = "AplhaShortcut"
+			end
+
+			dashboard.section.header.opts.hl = "AlphaHeader"
+			dashboard.section.buttons.opts.hl = "AlphaButtons"
+			dashboard.section.footer.opts.hl = "AlphaFooter"
+			dashboard.opts.layout[1].val = 8
+
+			return dashboard
+		end,
+		config = function(_, dashboard)
+			if vim.o.filetype == "lazy" then
+				vim.cmd.close()
+				vim.api.nvim_create_autocmd("User", {
+					pattern = "AphaReady",
+					callback = function()
+						require("lazy").show()
+					end,
+				})
+			end
+
+			require("alpha").setup(dashboard.opts)
+
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "LazyVimStarted",
+				callback = function()
+					local stats = require("lazy").stats()
+					local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+					dashboard.section.footer.val = "⚡ Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
+					pcall(vim.cmd.AlphaRedraw)
+				end,
+			})
+		end,
+	},
 })
 
 vim.diagnostic.config({
